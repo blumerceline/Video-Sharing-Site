e945c74812824a0e10d5ca80b0960f81facbd496
blumerceline <blumerceline@gmail.com>
2022-10-07 13:30:40 +1000

    got api working

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-    method: 'GET',
-    url: BASE_URL,
     params: {
       maxResults: '50'
     },
@@ -15,7 +13,7 @@ const options = {
   };
 
   export const fetchFromAPI = async (url) => {
-    const {data} = await axios.get(`${BASE_URL}/${url}`, options);
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
     return data;
   }
